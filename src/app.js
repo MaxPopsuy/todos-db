793e70d0a3e562188271183a297a7526a50ceb5a
Max Popsuy <maxim09p@gmail.com>
2022-07-19 19:26:35 +0300

    added basical web interface

--- src/app.js
+++ src/app.js
@@ -8,6 +8,9 @@ const errorHandler = require("./middlewares/errorHandler");
 
 const app = express();
 
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("db connected"))
@@ -20,6 +23,13 @@ app.use(volleyball);
 app.use(express.json());
 
 app.use("/api/v1", apiRouter);
+
+app.get("/", (req, res) => {
+  res.render('index')
+});
+app.get("/todos", (req, res) => {
+  res.render('todos')
+});
 
 app.use(errorHandler);
 
